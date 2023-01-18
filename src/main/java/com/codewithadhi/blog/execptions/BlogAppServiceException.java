c23e7496ed6466af6c70afe4b5b852913dd59845
saitejaadhi <111169979+saitejaadhi@users.noreply.github.com>
2023-01-18 14:50:22 +0530

    1st release (#8)
    * formatted code
    
    * formatted code (#3)
    
    * Create CODEOWNERS (#4)
    
    * done with basic things
    
    * Added common exception handler
    
    * Yes
    
    * added validations in UserRtabDTO

--- src/main/java/com/codewithadhi/blog/execptions/BlogAppServiceException.java
+++ src/main/java/com/codewithadhi/blog/execptions/BlogAppServiceException.java
@@ -2,22 +2,26 @@ package com.codewithadhi.blog.execptions;
 
 import org.springframework.http.HttpStatus;
 
-public class BlogAppServiceException extends RuntimeException{
+public class BlogAppServiceException extends RuntimeException {
     private final HttpStatus httpStatus;
-    public BlogAppServiceException(String message, Throwable throwable, HttpStatus httpStatus){
+
+    public BlogAppServiceException(String message, Throwable throwable, HttpStatus httpStatus) {
         super(message, throwable);
         this.httpStatus = httpStatus;
     }
+
     public BlogAppServiceException(String message, HttpStatus httpStatus, Throwable throwable) {
         super(message, throwable);
         this.httpStatus = httpStatus;
     }
-    public BlogAppServiceException(String message, HttpStatus httpStatus){
+
+    public BlogAppServiceException(String message, HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;
     }
+
     public BlogAppServiceException(String message) {
-        super (message);
+        super(message);
         this.httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
     }
 }
--- src/main/java/com/codewithadhi/blog/service/dto/UserRtabDTO.java
+++ src/main/java/com/codewithadhi/blog/service/dto/UserRtabDTO.java
@@ -1,6 +1,9 @@
 package com.codewithadhi.blog.service.dto;
 
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Builder
@@ -12,8 +15,15 @@ import lombok.*;
 public class UserRtabDTO {
     @Id
     private Long id;
+    @NotNull
+    @NotNull
     private String name;
+    @Email
+    @NotNull
+    @Size(max = 20)
     private String email;
+    @NotNull
+    @Size(min = 8, max = 20)
     private String password;
     private String about;
 }
